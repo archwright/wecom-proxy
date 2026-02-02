81e6b364c756d8a588e9c456af67024d9ff8209c
archwright <147500319+archwright@users.noreply.github.com>
2026-02-02 14:47:45 +0000

    Update server.js

--- server.js
+++ server.js
@@ -72,4 +72,10 @@ app.post("/wecom/callback", async (req, reply) => {
 });
 
 const port = process.env.PORT ? Number(process.env.PORT) : 8080;
-app.lis
+
+try {
+  await app.listen({ port, host: "0.0.0.0" });
+} catch (err) {
+  app.log.error(err);
+  process.exit(1);
+}
